dded311bc5524beeec2cf217643186be99958666
Daniel Casvill <casvilldaniel@gmail.com>
2025-12-08 20:56:55 -0500

    Endpoint para listar matriculas Global añadido

--- enrollments/urls.py
+++ enrollments/urls.py
@@ -6,7 +6,7 @@ from .views.matricula_views import RegistrarMatriculaView, ListadoGlobalMatricul
 urlpatterns = [
     path('matriculas/', RegistrarMatriculaView.as_view()),          # GET y POST
     path('matriculas/<int:pk>/', RegistrarMatriculaView.as_view()), # PUT y DELETE
-    path('matriculas/listar/', ListadoGlobalMatriculasView.as_view()), 
+    path('matriculas/listado-global/', ListadoGlobalMatriculasView.as_view()), 
     path('matriculas/<int:pk>/', RegistrarMatriculaView.as_view(), name='matricula-update'),
 
 ]
